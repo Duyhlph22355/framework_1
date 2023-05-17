4a266583a9f3c58c51808ce570d1e9c2cbc28d0a
Duyhlph22355 <115914772+Duyhlph22355@users.noreply.github.com>
2023-05-17 17:01:08 +0700

    sua lai thu muc

--- Assignment/src/app/app-routing.module.ts
+++ Assignment/src/app/app-routing.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './pages/home/home.component';
-import { LoginComponent } from './pages/login/login.component';
-import { RegisterComponent } from './pages/register/register.component';
-import { ProductDetailComponent } from './pages/product-detail/product-detail.component';
+import { HomeComponent } from './pages/user/home/home.component';
+import { LoginComponent } from './pages/user/login/login.component';
+import { RegisterComponent } from './pages/user/register/register.component';
+import { ProductDetailComponent } from './pages/user/product-detail/product-detail.component';
 
 const routes: Routes = [
   {path: "", component: HomeComponent},
--- Assignment/src/app/app.module.ts
+++ Assignment/src/app/app.module.ts
@@ -4,12 +4,12 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { HomeComponent } from './pages/home/home.component';
-import { LoginComponent } from './pages/login/login.component';
-import { RegisterComponent } from './pages/register/register.component';
-import { ProductDetailComponent } from './pages/product-detail/product-detail.component';
+import { HeaderComponent } from './components/user/header/header.component';
+import { FooterComponent } from './components/user/footer/footer.component';
+import { HomeComponent } from './pages/user/home/home.component';
+import { LoginComponent } from './pages/user/login/login.component';
+import { RegisterComponent } from './pages/user/register/register.component';
+import { ProductDetailComponent } from './pages/user/product-detail/product-detail.component';
 
 @NgModule({
   declarations: [
